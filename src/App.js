775d5b48dee8ab309fc5a06332f204252df482a6
Alena.Kygai <alena.kygai@gmail.com>
2023-07-11 17:45:44 +0300

    Add changeValue

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 function App() {
 
   const [likes,setLikes] = useState(0);
+  const [value,setValue] = useState("Initial value");
 
-  const increment = () => setLikes(likes+1)
-  const decrement = () => setLikes(likes-1)
+  const increment = () => setLikes(likes+1);
+  const decrement = () => setLikes(likes-1);
+
+  const changeValue = (e) => setValue(e.target.value);
 
   return (
     <div className="App">
       <h2>{likes}</h2>
+      <h2>{value}</h2>
+      <input 
+        type='text' 
+        value={value} 
+        onChange={changeValue}
+      />
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>
